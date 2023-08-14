2e65c32fdf3c3c3e90a70e8aa0d0935c2f87ffca
Quoc Thinh <lequocthinh401@gmail.com>
2023-08-14 20:09:18 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTTH_Buoi3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoNguyen();
+            Console.ReadKey();
+        }
+        public static int SoNguyen()
+        {
+            Console.Write("Hay nhap vao so nguyen n: ");
+            int n =int.Parse(Console.ReadLine());
+            return n;   
+        }
+    }
+}
